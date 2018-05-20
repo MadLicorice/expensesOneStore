cb19717ea8901920290ffa8fe43e22fad8e7abf4
MadLicorice <madlicorice101@gmail.com>
2018-05-20 14:45:01 -0700

    Implement simple actions

--- expensify/src/playground/redux-102.js
+++ expensify/src/playground/redux-102.js
@@ -1,11 +1,43 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: state.count = 0
+      };
+    default:
+      return state;
+  }
 });
 
 console.log(store.getState());
 
-// Next up: actions
-// I'd like to increment the count
-// I'd like to reset the count to zero
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+console.log(store.getState());
+
+
+// TODO: watching changes and dynamic actions
