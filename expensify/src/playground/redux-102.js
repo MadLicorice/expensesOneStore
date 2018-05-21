624ffe1372e21821e8842aa4a1ed8fe5226de018
MadLicorice <madlicorice101@gmail.com>
2018-05-20 19:45:09 -0700

    Implement action generators

--- expensify/src/playground/redux-102.js
+++ expensify/src/playground/redux-102.js
@@ -1,23 +1,39 @@
 import { createStore } from 'redux';
 
-// TODO: action generators
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy,
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy,
+});
+
+const setCount = ({ count } = {}) => ({
+  type: 'SET',
+  count,
+});
+
+const resetCount = ({} = {}) => ({
+  type: 'RESET',
+})
+
 
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
     case 'SET':
       return {
         count: action.count
-      }
+      };
     case 'RESET':
       return {
         count: 0
@@ -31,31 +47,16 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 })
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+// unsubscribe(); // this will unsub us from doing an action anytime the state changes
 
-// unsubscribe();
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-store.dispatch({
-  type: 'INCREMENT'
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'RESET'
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: 'DECREMENT'
-});
+store.dispatch(decrementCount({ decrementBy: 3}));
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-})
+store.dispatch(setCount({ count: 57}));
