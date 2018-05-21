ad7f7cefd771029c8982a077ed85069372c41616
MadLicorice <madlicorice101@gmail.com>
2018-05-20 18:50:56 -0700

    Implement more actions, todo action generators

--- expensify/src/playground/redux-102.js
+++ expensify/src/playground/redux-102.js
@@ -1,29 +1,42 @@
 import { createStore } from 'redux';
 
+// TODO: action generators
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       };
+    case 'SET':
+      return {
+        count: action.count
+      }
     case 'RESET':
       return {
-        count: state.count = 0
+        count: 0
       };
     default:
       return state;
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+})
 
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 });
+
+// unsubscribe();
 
 store.dispatch({
   type: 'INCREMENT'
@@ -34,10 +47,15 @@ store.dispatch({
 });
 
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'DECREMENT'
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+});
 
-
-// TODO: watching changes and dynamic actions
+store.dispatch({
+  type: 'SET',
+  count: 101
+})
